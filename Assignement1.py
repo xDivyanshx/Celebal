cfb058cc8dd72462ca6b6b83507b859fdd4ec82c
xDivyanshx <135411867+xDivyanshx@users.noreply.github.com>
2024-06-07 22:23:32 +0530

    Add files via upload

---
+++ Assignement1.py
@@ -0,0 +1,20 @@
+def lower_triangular(n):
+    print("Lower Triangular:")
+    for i in range(1, n + 1):
+        print('* ' * i)
+    print()
+def upper_triangular(n):
+    print("Upper Triangular:")
+    for i in range(n):
+        print('  ' * i + '* ' * (n - i))
+    print() 
+def pyramid(n):
+    print("Pyramid:")
+    for i in range(n):
+        print(' ' * (n - i - 1) + '* ' * (i + 1))
+    print()
+#n=int(input("Enter n."))
+n=5
+lower_triangular(n)
+upper_triangular(n)
+pyramid(n)
